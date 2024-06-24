e9801595d701e4fdc44df03549b2b0611298ef86
Gentility01 <mastergentility5@gmail.com>
2024-06-24 19:03:27 +0100

    Fixes

--- render.sh
+++ render.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-set -o errexit
-set -o pipefail
-set -o nounset
+
 # Your series of commands
-# exec python manage.py migrate
+exec python manage.py migrate
 # exec /usr/local/bin/gunicorn config.asgi --bind 0.0.0.0:5000 --chdir=/app -k uvicorn.workers.UvicornWorker
 exec python manage.py runserver 0.0.0.0:5000
